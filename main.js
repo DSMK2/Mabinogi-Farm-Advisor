8acc9bf54b4693116b9f47c7e045cd1ec397fa9f
Aric Ng <dropshipmk2@gmail.com>
2024-04-16 17:28:23 -0400

    Time update stuff

--- main.js
+++ main.js
@@ -14,6 +14,35 @@ document.addEventListener('DOMContentLoaded', function () {
       return false;
     }
   })();
+  var getElapsedHours = (function () {
+    var DOMFormTimeValues = document.querySelector('.farm__form--time');
+    var DOMDate = DOMFormTimeValues.time_date;
+    var DOMHour = DOMFormTimeValues.time_hour;
+    var DOMMin = DOMFormTimeValues.time_minute;
+    var dateStart;
+
+    // Based on PST time
+    function updateDateStart() {
+      dateStart = new Date(DOMDate.value + ' ' + DOMHour.value.padStart(2, '0') + ':' + DOMMin.value.padStart(2, '0') + ' GMT-07:00');
+    }
+
+    DOMFormTimeValues.addEventListener('change', function () {
+      updateDateStart();
+    });
+
+    updateDateStart();
+
+    return function () {
+      var dateCurrent = new Date();
+      var timeDiff = dateCurrent.getTime() - dateStart.getTime();
+
+      if (isNaN(timeDiff)) {
+        return 0;
+      }
+
+      return Math.floor(timeDiff / 3600000);
+    }
+  })();
 
   /////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////
@@ -22,10 +51,10 @@ document.addEventListener('DOMContentLoaded', function () {
   /////////////////////////////////////////////////////////////////////////////////
   (function () {
     // DOM
-    var DOMForm = document.querySelector('.farm__form');
+    var DOMFormValues = document.querySelector('.farm__form--values');
     var DOMCrops = Array.prototype.slice.call(document.querySelectorAll('.farm__crop'), 0);
     var DOMCropLabels = Array.prototype.slice.call(document.querySelectorAll('.farm__crop-label'), 0);
-    var DOMCropSelector = DOMForm.querySelector('[name="the_crop"]');
+    var DOMCropSelector = DOMFormValues.querySelector('[name="the_crop"]');
     var DOMRate = document.querySelector('.farm__growth-rate');
     var DOMTodos = Array.prototype.slice.call(document.querySelectorAll('.todo'), 0);
     // Vars
@@ -63,9 +92,9 @@ document.addEventListener('DOMContentLoaded', function () {
     };
     var cropInputs = {
       crop: '',
-      h2o: 0,
-      fert: 0,
-      bug: 0
+      h2o: 40,
+      fert: 30,
+      bug: 80
     };
 
     /////////////////////////////////////////////////////////////////////////////////
@@ -100,15 +129,15 @@ document.addEventListener('DOMContentLoaded', function () {
       cropInputs.fert = farmVals.fert;
       cropInputs.bug = farmVals.bug;
 
-      DOMForm.the_h2o.value = cropInputs.h2o;
-      DOMForm.the_fert.value = cropInputs.fert;
-      DOMForm.the_bug.value = cropInputs.bug;
+      DOMFormValues.the_h2o.value = cropInputs.h2o;
+      DOMFormValues.the_fert.value = cropInputs.fert;
+      DOMFormValues.the_bug.value = cropInputs.bug;
 
       // Insure form values are updated
       DOMCropSelector.dispatchEvent(new Event('change'));
-      DOMForm.the_h2o.dispatchEvent(new Event('change'));
-      DOMForm.the_fert.dispatchEvent(new Event('change'));
-      DOMForm.the_bug.dispatchEvent(new Event('change'));
+      DOMFormValues.the_h2o.dispatchEvent(new Event('change'));
+      DOMFormValues.the_fert.dispatchEvent(new Event('change'));
+      DOMFormValues.the_bug.dispatchEvent(new Event('change'));
 
       updateTodo();
     }
@@ -132,6 +161,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     /////////////////////////////////////////////////////////////////////////////////
     // END: Local Storage Handling
+    /////////////////////////////////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////////////////////////////////
+    // BEGIN: Basic Features
     /////////////////////////////////////////////////////////////////////////////////
     function resetCrops() {
       DOMCrops.forEach(function (DOMCrop) {
@@ -186,17 +219,47 @@ document.addEventListener('DOMContentLoaded', function () {
 
       DOMRate.innerHTML = growthRate.toFixed(2);
     }
+    /////////////////////////////////////////////////////////////////////////////////
+    // END: Basic Features
+    /////////////////////////////////////////////////////////////////////////////////
+
+    window.updateCropTime = function () {
+      var elapsedHours = 0;
+      var newH2O = 0;
+      var newFert = 0;
+      var newBug = 0;
+
+      if (!cropInputs.crop) {
+        console.error('No crop selected');
+        return;
+      }
+
+      elapsedHours = getElapsedHours();
+
+      newH2O = Math.floor(DOMFormValues.the_h2o.value - cropValues[cropInputs.crop].dehydration * elapsedHours);
+      newFert = Math.floor(DOMFormValues.the_fert.value - cropValues[cropInputs.crop].malnourishment * elapsedHours);
+      newBug = Math.floor(DOMFormValues.the_bug.value - cropValues[cropInputs.crop].bug * elapsedHours);
+
+      DOMFormValues.the_h2o.value = newH2O < 0 ? 0 : newH2O;
+      DOMFormValues.the_fert.value = newFert < 0 ? 0 : newFert;
+      DOMFormValues.the_bug.value = newBug < 0 ? 0 : newBug;
+
+      DOMFormValues.the_h2o.dispatchEvent(new Event('change'));
+      DOMFormValues.the_fert.dispatchEvent(new Event('change'));
+      DOMFormValues.the_bug.dispatchEvent(new Event('change'));
+    }
+
 
     /////////////////////////////////////////////////////////////////////////////////
     // BEGIN: Events
     /////////////////////////////////////////////////////////////////////////////////
-    DOMForm.addEventListener('change', function (e) {
+    DOMFormValues.addEventListener('change', function (e) {
       e.preventDefault();
 
       cropInputs.crop = DOMCropSelector.querySelector(':checked').value;
-      cropInputs.h2o = DOMForm.the_h2o.value;
-      cropInputs.fert = DOMForm.the_fert.value;
-      cropInputs.bug = DOMForm.the_bug.value;
+      cropInputs.h2o = DOMFormValues.the_h2o.value;
+      cropInputs.fert = DOMFormValues.the_fert.value;
+      cropInputs.bug = DOMFormValues.the_bug.value;
 
       saveData();
       updateTodo();
@@ -254,16 +317,16 @@ document.addEventListener('DOMContentLoaded', function () {
       cropInputs.fert = 30;
       cropInputs.bug = 80;
 
-      DOMForm.the_h2o.value = cropInputs.h2o;
-      DOMForm.the_fert.value = cropInputs.fert;
-      DOMForm.the_bug.value = cropInputs.bug;
+      DOMFormValues.the_h2o.value = cropInputs.h2o;
+      DOMFormValues.the_fert.value = cropInputs.fert;
+      DOMFormValues.the_bug.value = cropInputs.bug;
 
       DOMRate.innerHTML = '0.00';
 
       DOMCropSelector.dispatchEvent(new Event('change'));
-      DOMForm.the_h2o.dispatchEvent(new Event('change'));
-      DOMForm.the_fert.dispatchEvent(new Event('change'));
-      DOMForm.the_bug.dispatchEvent(new Event('change'));
+      DOMFormValues.the_h2o.dispatchEvent(new Event('change'));
+      DOMFormValues.the_fert.dispatchEvent(new Event('change'));
+      DOMFormValues.the_bug.dispatchEvent(new Event('change'));
     });
 
     window.addEventListener('blur', function () {
